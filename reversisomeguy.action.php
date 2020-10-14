411a33129218b308e5749327e83a2eac624342a2
Jonathan Moyett <someguy590@gmail.com>
2020-10-14 17:34:26 -0500

    add play disc call

--- reversisomeguy.action.php
+++ reversisomeguy.action.php
@@ -37,27 +37,12 @@ class action_reversisomeguy extends APP_GameAction
     }
   }
 
-  // TODO: defines your action entry points there
-
-
-  /*
-    
-    Example:
-  	
-    public function myAction()
-    {
-        self::setAjaxMode();     
-
-        // Retrieve arguments
-        // Note: these arguments correspond to what has been sent through the javascript "ajaxcall" method
-        $arg1 = self::getArg( "myArgument1", AT_posint, true );
-        $arg2 = self::getArg( "myArgument2", AT_posint, true );
-
-        // Then, call the appropriate method in your game logic, like "playCard" or "myAction"
-        $this->game->myAction( $arg1, $arg2 );
-
-        self::ajaxResponse( );
-    }
-    
-    */
+  public function playDisc()
+  {
+    self::setAjaxMode();
+    $x = self::getArg("x", AT_posint, true);
+    $y = self::getArg("y", AT_posint, true);
+    $result = $this->game->playDisc($x, $y);
+    self::ajaxResponse();
+  }
 }
